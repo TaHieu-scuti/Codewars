b4f00b4973e37a0c01bac574c5f424e1e0923afc
hieutq-scuti <hieutq@scuti.asia>
2017-12-02 01:31:05 +0700

    make function paginationHelper and sum strings as numbers

---
+++ 4kyu_sumStringAsNumbers.js
@@ -0,0 +1,26 @@
+function sumStrings(a,b) {
+    var num1 = a, num2 = b;
+    while (num1.length > 1 && num1[0] === '0')
+        num1 = num1.slice(1);
+
+    while (num2.length > 1 && num2[0] === '0')
+        num2 = num2.slice(1);
+
+    num1 = num1.split('').reverse().join('');
+    num2 = num2.split('').reverse().join('');
+
+    var add = 0
+        , ans = '';
+    for (var i = 0, len = num1.length > num2.length ? num1.length : num2.length; i < len; i++) {
+        var a = i < num1.length ? Number(num1[i]) : 0
+            , b = i < num2.length ? Number(num2[i]) : 0;
+
+        var c = a + b + add;
+        ans += c % 10;
+        add = c >= 10 ? 1 : 0;
+    }
+
+    if (add) ans += add;
+
+    return ans.split('').reverse().join('');
+}
